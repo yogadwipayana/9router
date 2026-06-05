a2ee487d79b3cdedddb5384cacc1c53ba0839347
yogadwipayana <yogadwipayana2006@gmail.com>
2026-06-05 17:05:57 +0800

    fix deployment error when npm run start

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ git pull
 npm install
 npx prisma generate
 npm run build
-PORT=4000 pm2 start npm --name "9router" -- run start
+
+cp -r public .next/standalone/
+cp -r .next/static .next/standalone/.next/
+
+PORT=4000 pm2 start node --name "9router" -- .next/standalone/server.js
 
 pm2 save
